f0e3eeccd80963d5e318513806ccefb4c8156568
Rob-Yoo <jin12243@naver.com>
2024-06-08 19:21:48 +0900

    Observable의 Create, Subscribe, Disposable 사용 연습

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -36,15 +36,62 @@ class ViewController: UIViewController {
 
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
 
+    // Observable의 생명주기
+    // 1. Create
+    // 2. Subscribe (비동기 코드 실행)
+    // 3. onNext (데이터 전달)
+
+    // 4번 혹은 5번 상태일 때 동작 끝(subscribe가 재호출되기 전까지 재사용 X) -> Observable의 클로저가 소멸됨
+    // 4. onCompleted / onError
+    // 5. Disposed
+    
+    
+    func downloadJson(url: String) -> Observable<String?> {
+        // 비동기로 생기는 데이터를 Observable로 감싸서 리턴하는 방법
+        return Observable.create() { emitter in
+            let url = URL(string: url)!
+            let task = URLSession.shared.dataTask(with: url) { (data, _, error) in
+                guard error == nil else {
+                    emitter.onError(error!)
+                    return
+                }
+                
+                if let rawData = data, let json = String(data: rawData, encoding: .utf8) {
+                    emitter.onNext(json)
+                }
+                
+                emitter.onCompleted()
+            }
+            
+            task.resume()
+            
+            return Disposables.create() {
+                task.cancel()
+            }
+        }
+    }
+    
     @IBAction func onLoad() {
         editView.text = ""
         setVisibleWithAnimation(activityIndicator, true)
 
-        let url = URL(string: MEMBER_LIST_URL)!
-        let data = try! Data(contentsOf: url)
-        let json = String(data: data, encoding: .utf8)
-        self.editView.text = json
+        // 2. Observable로 오는 데이터를 받아서 처리하는 방법
+        let observable = downloadJson(url: MEMBER_LIST_URL) // 비동기 코드 실행 전
         
-        self.setVisibleWithAnimation(self.activityIndicator, false)
+        let disposable = observable.subscribe { event in // subscribe 오퍼레이터가 호출되면 observable의 비동기 코드 실행
+                switch event {
+                case .next(let json):
+                    DispatchQueue.main.async {
+                        self.editView.text = json
+                        self.setVisibleWithAnimation(self.activityIndicator, false)
+                    }
+                case .error(_):
+                    break
+                case .completed:
+                    break
+                }
+            }
+        
+//        disposable.dispose() -> 비동기 코드 실행 중간에 취소시킬 수 있음
     }
 }
